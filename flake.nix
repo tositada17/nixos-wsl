d406f0a351c296d81477efd9925f60cdc8a1e437
tositada17 <tositada17@gmail.com>
2024-12-26 10:50:17 +0000

    add home-manager

--- flake.nix
+++ flake.nix
@@ -2,19 +2,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+  outputs = inputs@{ self, nixpkgs, nixos-wsl, home-manager, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           { nix.registry.nixpkgs.flake = nixpkgs; }
           ./configuration.nix
+          
+          #wsl-setting
           nixos-wsl.nixosModules.default
           {
             system.stateVersion = "24.05";
             wsl.enable = true;
+          }
+          
+          # home-manager settings
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nixos = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
           }
         ];
       };
