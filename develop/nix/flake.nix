b062e26aa34911ff66a5c6bc203186795a520e04
tositada17 <tositada17@gmail.com>
2025-01-15 06:13:34 +0000

    add nixd develop

---
+++ develop/nix/flake.nix
@@ -0,0 +1,18 @@
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }: 
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShellNoCC {
+      packages = with pkgs;[
+        nixd
+      ];
+    };
+  };
+}
