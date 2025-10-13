826f86a26f5e4eab1b3e285a6b872ffb8b89b617
tositada17 <tositada17@gmail.com>
2025-10-13 11:59:16 +0000

    init tenv

--- develop/gcloud/gcloud-sdk.nix
+++ develop/gcloud/gcloud-sdk.nix
@@ -2,15 +2,25 @@
 
 
 let
-  gke = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
+  gcloud-components = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
     gke-gcloud-auth-plugin
+    config-connector
   ]);
 in
 
 pkgs.mkShell{
-  packages = with pkgs; [
+  packages = [
     pkgs.google-cloud-sdk
-    gke
+    gcloud-components
     pkgs.terraform-docs
   ];
+
+  shellHook = ''
+    export tfversion="1.13.3"
+    export projectname="gcloud"
+
+    export PS1="\n($projectname) $PS1"
+    tenv tf install $tfversion
+    tenv tf use $tfversion
+  '';
 }
